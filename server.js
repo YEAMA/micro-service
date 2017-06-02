630f2e667252f47ff0852033a0d189a3cd3436a9
YEAMA <youssefemad99@hotmail.com>
2017-06-02 23:51:34 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const ip = require('ip')
+
+const port = process.env.PORT || 3000
+
+var app = express()
+
+app.get('/', (req, res) => {
+  var ipaddress = ip.address();
+  var language = req.headers['accept-language'];
+  var software = req.headers['user-agent'];
+  var result = JSON.stringify({
+      ipaddress, language, software
+  }, undefined, 4);
+  
+  res.send(result);
+})
+
+app.listen(port, () => {
+    console.log(`Started on port ${port}`)
+})
