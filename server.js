b489e5ce44e8f01556d888798133a8fe655a76ff
YEAMA <youssefemad99@hotmail.com>
2017-06-03 15:24:35 +0200

    Final edit

--- server.js
+++ server.js
@@ -6,14 +6,16 @@ const port = process.env.PORT || 3000
 var app = express()
 
 app.get('/', (req, res) => {
-  var ipaddress = ip.address();
-  var language = req.headers['accept-language'];
-  var software = req.headers['user-agent'];
-  var result = JSON.stringify({
-      ipaddress, language, software
-  }, undefined, 4);
-  
-  res.send(result);
+    var ipaddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    var language = req.headers['accept-language'];
+    var software = req.headers['user-agent'];
+    var result = JSON.stringify({
+        ipaddress,
+        language,
+        software
+    }, undefined, 4);
+
+    res.send(result);
 })
 
 app.listen(port, () => {
